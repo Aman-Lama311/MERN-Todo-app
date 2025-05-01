82a0afe5822f788a0ea44075c67a366318b993a5
Aman-Lama311 <lamaaman2036@gmail.com>
2025-05-01 13:23:44 +0545

    other

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://mern-todo-app-c4ui.vercel.app",
+    origin: "https://mern-todo-app-c4ui.vercel.app/",
     credentials: true,
   })
 );
