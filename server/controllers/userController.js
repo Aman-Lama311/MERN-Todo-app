29b7e8aa02292ab90977489304474b3ad739f914
Aman-Lama311 <lamaaman2036@gmail.com>
2025-05-01 22:21:42 +0545

    pushing

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -69,6 +69,7 @@ export const login = async (req, res) => {
       .status(200)
       .cookie("token", token, {
         httpOnly: true,
+        secure: true,
         sameSite: "strict",
         maxAge: 24 * 60 * 60 * 1000,
       })
