69442c6805539d26d2cba919fe65e2416d27b4f3
Aman-Lama311 <lamaaman2036@gmail.com>
2025-05-02 08:29:45 +0545

    oh man finally

--- server/models/User.js
+++ server/models/User.js
@@ -1,19 +1,19 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   fullName: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
-    required: true
+    required: true,
   },
   password: {
     type: String,
-    required: true
-  }
+    required: true,
+    minimum: 6,
+  },
 });
 
-
-export const User = mongoose.model('User', userSchema);
+export const User = mongoose.model("User", userSchema);
